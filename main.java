5a71ca670bcec84ee225dc7f0e5fcc5d13535fc2
Brandon Weathers <brandon.r.weathers@gmail.com>
2024-06-17 16:30:03 -0400

    addedPrintingMethodsAndUpdatedConstructor

--- main.java
+++ main.java
@@ -1,6 +1,20 @@
-class main{
+public class main{
 	public static void main (String args []){
-		box myBox = new box(1, 2, 3, 4, 5, 6, 7, 8, 9);
-		myBox.printBox();
+		box myBoxOne = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+		box myBoxTwo = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+		box myBoxThree = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+		box myBoxFour = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+		box myBoxFive = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+		box myBoxSix = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+		box myBoxSeven = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+		box myBoxEight = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+		box myBoxNine = new box(0, 1, 2, 3, 4, 5, 6, 7, 8);
+
+		grid myGrid = new grid(myBoxOne, myBoxTwo, myBoxThree, myBoxFour, myBoxFive, myBoxSix, myBoxSeven, myBoxEight, myBoxNine);
+
+		myGrid.printGrid();
+
+		// grid testGrid = new grid();
+		// testGrid.printGrid();
 	}
 }
