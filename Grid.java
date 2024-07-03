16d94bc13654f2a9c13405da1932ca001c18b8e5
Brandon Weathers <brandon.r.weathers@gmail.com>
2024-07-03 14:31:44 -0400

    small updates to the readme and info file

--- Grid.java
+++ Grid.java
@@ -11,114 +11,120 @@ class Grid{
 	// Creating a grid with custom inputs
 	public Grid(Box boxZero, Box boxOne, Box boxTwo, Box boxThree, Box boxFour, Box boxFive, Box boxSix, Box boxSeven, Box boxEight){
 		for(int boxesIndex = 0; boxesIndex <= 8; boxesIndex++){
-			     if(boxesIndex == 0) Boxes[0] = boxZero;
-			else if(boxesIndex == 1) Boxes[1] = boxOne;
-			else if(boxesIndex == 2) Boxes[2] = boxTwo;
-			else if(boxesIndex == 3) Boxes[3] = boxThree;
-			else if(boxesIndex == 4) Boxes[4] = boxFour;
-			else if(boxesIndex == 5) Boxes[5] = boxFive;
-			else if(boxesIndex == 6) Boxes[6] = boxSix;
-			else if(boxesIndex == 7) Boxes[7] = boxSeven;
-			else if(boxesIndex == 8) Boxes[8] = boxEight;
+			     if(boxesIndex == 0) Boxes[boxesIndex] = boxZero;
+			else if(boxesIndex == 1) Boxes[boxesIndex] = boxOne;
+			else if(boxesIndex == 2) Boxes[boxesIndex] = boxTwo;
+			else if(boxesIndex == 3) Boxes[boxesIndex] = boxThree;
+			else if(boxesIndex == 4) Boxes[boxesIndex] = boxFour;
+			else if(boxesIndex == 5) Boxes[boxesIndex] = boxFive;
+			else if(boxesIndex == 6) Boxes[boxesIndex] = boxSix;
+			else if(boxesIndex == 7) Boxes[boxesIndex] = boxSeven;
+			else if(boxesIndex == 8) Boxes[boxesIndex] = boxEight;
 		}
 	}
 
-	public boolean checkRow(int rowIndex){
+	public boolean alternateCheckRow(wantedRow){
+		int[] numbersInRow = new int[9];
+		// Need to check with father to see if there
+		// is a better way to implement this method
+	}
+
+	public boolean checkRow(int wantedRow){
 		// Will return false if all the elements in the given row are the same
-		int[] currentRow = new int[9];
-		if(rowIndex == 0){
-			currentRow[0] = Boxes[0].getCellValue(0);
-			currentRow[1] = Boxes[0].getCellValue(1);
-			currentRow[2] = Boxes[0].getCellValue(2);
-			currentRow[3] = Boxes[1].getCellValue(0);
-			currentRow[4] = Boxes[1].getCellValue(1);
-			currentRow[5] = Boxes[1].getCellValue(2);
-			currentRow[6] = Boxes[2].getCellValue(0);
-			currentRow[7] = Boxes[2].getCellValue(1);
-			currentRow[8] = Boxes[2].getCellValue(2);
-		}else if(rowIndex == 1){
-			currentRow[0] = Boxes[0].getCellValue(3);
-			currentRow[1] = Boxes[0].getCellValue(4);
-			currentRow[2] = Boxes[0].getCellValue(5);
-			currentRow[3] = Boxes[1].getCellValue(3);
-			currentRow[4] = Boxes[1].getCellValue(4);
-			currentRow[5] = Boxes[1].getCellValue(5);
-			currentRow[6] = Boxes[2].getCellValue(3);
-			currentRow[7] = Boxes[2].getCellValue(4);
-			currentRow[8] = Boxes[2].getCellValue(5);
-		}else if(rowIndex == 2){
-			currentRow[0] = Boxes[0].getCellValue(6);
-			currentRow[1] = Boxes[0].getCellValue(7);
-			currentRow[2] = Boxes[0].getCellValue(8);
-			currentRow[3] = Boxes[1].getCellValue(6);
-			currentRow[4] = Boxes[1].getCellValue(7);
-			currentRow[5] = Boxes[1].getCellValue(8);
-			currentRow[6] = Boxes[2].getCellValue(6);
-			currentRow[7] = Boxes[2].getCellValue(7);
-			currentRow[8] = Boxes[2].getCellValue(8);
-		}else if(rowIndex == 3){
-			currentRow[0] = Boxes[3].getCellValue(0);
-			currentRow[1] = Boxes[3].getCellValue(1);
-			currentRow[2] = Boxes[3].getCellValue(2);
-			currentRow[3] = Boxes[4].getCellValue(0);
-			currentRow[4] = Boxes[4].getCellValue(1);
-			currentRow[5] = Boxes[4].getCellValue(2);
-			currentRow[6] = Boxes[5].getCellValue(0);
-			currentRow[7] = Boxes[5].getCellValue(1);
-			currentRow[8] = Boxes[5].getCellValue(2);
-		}else if(rowIndex == 4){
-			currentRow[0] = Boxes[3].getCellValue(3);
-			currentRow[1] = Boxes[3].getCellValue(4);
-			currentRow[2] = Boxes[3].getCellValue(5);
-			currentRow[3] = Boxes[4].getCellValue(3);
-			currentRow[4] = Boxes[4].getCellValue(4);
-			currentRow[5] = Boxes[4].getCellValue(5);
-			currentRow[6] = Boxes[5].getCellValue(3);
-			currentRow[7] = Boxes[5].getCellValue(4);
-			currentRow[8] = Boxes[5].getCellValue(5);
-		}else if(rowIndex == 5){
-			currentRow[0] = Boxes[3].getCellValue(6);
-			currentRow[1] = Boxes[3].getCellValue(7);
-			currentRow[2] = Boxes[3].getCellValue(8);
-			currentRow[3] = Boxes[4].getCellValue(6);
-			currentRow[4] = Boxes[4].getCellValue(7);
-			currentRow[5] = Boxes[4].getCellValue(8);
-			currentRow[6] = Boxes[5].getCellValue(6);
-			currentRow[7] = Boxes[5].getCellValue(7);
-			currentRow[8] = Boxes[5].getCellValue(8);
-		}else if(rowIndex == 6){
-			currentRow[0] = Boxes[6].getCellValue(0);
-			currentRow[1] = Boxes[6].getCellValue(1);
-			currentRow[2] = Boxes[6].getCellValue(2);
-			currentRow[3] = Boxes[7].getCellValue(0);
-			currentRow[4] = Boxes[7].getCellValue(1);
-			currentRow[5] = Boxes[7].getCellValue(2);
-			currentRow[6] = Boxes[8].getCellValue(0);
-			currentRow[7] = Boxes[8].getCellValue(1);
-			currentRow[8] = Boxes[8].getCellValue(2);
-		}else if(rowIndex == 7){
-			currentRow[0] = Boxes[6].getCellValue(3);
-			currentRow[1] = Boxes[6].getCellValue(4);
-			currentRow[2] = Boxes[6].getCellValue(5);
-			currentRow[3] = Boxes[7].getCellValue(3);
-			currentRow[4] = Boxes[7].getCellValue(4);
-			currentRow[5] = Boxes[7].getCellValue(5);
-			currentRow[6] = Boxes[8].getCellValue(3);
-			currentRow[7] = Boxes[8].getCellValue(4);
-			currentRow[8] = Boxes[8].getCellValue(5);
+		int[] row = new int[9];
+		if(wantedRow == 0){
+			row[0] = Boxes[0].getCellValue(0);
+			row[1] = Boxes[0].getCellValue(1);
+			row[2] = Boxes[0].getCellValue(2);
+			row[3] = Boxes[1].getCellValue(0);
+			row[4] = Boxes[1].getCellValue(1);
+			row[5] = Boxes[1].getCellValue(2);
+			row[6] = Boxes[2].getCellValue(0);
+			row[7] = Boxes[2].getCellValue(1);
+			row[8] = Boxes[2].getCellValue(2);
+		}else if(wantedRow == 1){
+			row[0] = Boxes[0].getCellValue(3);
+			row[1] = Boxes[0].getCellValue(4);
+			row[2] = Boxes[0].getCellValue(5);
+			row[3] = Boxes[1].getCellValue(3);
+			row[4] = Boxes[1].getCellValue(4);
+			row[5] = Boxes[1].getCellValue(5);
+			row[6] = Boxes[2].getCellValue(3);
+			row[7] = Boxes[2].getCellValue(4);
+			row[8] = Boxes[2].getCellValue(5);
+		}else if(wantedRow == 2){
+			row[0] = Boxes[0].getCellValue(6);
+			row[1] = Boxes[0].getCellValue(7);
+			row[2] = Boxes[0].getCellValue(8);
+			row[3] = Boxes[1].getCellValue(6);
+			row[4] = Boxes[1].getCellValue(7);
+			row[5] = Boxes[1].getCellValue(8);
+			row[6] = Boxes[2].getCellValue(6);
+			row[7] = Boxes[2].getCellValue(7);
+			row[8] = Boxes[2].getCellValue(8);
+		}else if(wantedRow == 3){
+			row[0] = Boxes[3].getCellValue(0);
+			row[1] = Boxes[3].getCellValue(1);
+			row[2] = Boxes[3].getCellValue(2);
+			row[3] = Boxes[4].getCellValue(0);
+			row[4] = Boxes[4].getCellValue(1);
+			row[5] = Boxes[4].getCellValue(2);
+			row[6] = Boxes[5].getCellValue(0);
+			row[7] = Boxes[5].getCellValue(1);
+			row[8] = Boxes[5].getCellValue(2);
+		}else if(wantedRow == 4){
+			row[0] = Boxes[3].getCellValue(3);
+			row[1] = Boxes[3].getCellValue(4);
+			row[2] = Boxes[3].getCellValue(5);
+			row[3] = Boxes[4].getCellValue(3);
+			row[4] = Boxes[4].getCellValue(4);
+			row[5] = Boxes[4].getCellValue(5);
+			row[6] = Boxes[5].getCellValue(3);
+			row[7] = Boxes[5].getCellValue(4);
+			row[8] = Boxes[5].getCellValue(5);
+		}else if(wantedRow == 5){
+			row[0] = Boxes[3].getCellValue(6);
+			row[1] = Boxes[3].getCellValue(7);
+			row[2] = Boxes[3].getCellValue(8);
+			row[3] = Boxes[4].getCellValue(6);
+			row[4] = Boxes[4].getCellValue(7);
+			row[5] = Boxes[4].getCellValue(8);
+			row[6] = Boxes[5].getCellValue(6);
+			row[7] = Boxes[5].getCellValue(7);
+			row[8] = Boxes[5].getCellValue(8);
+		}else if(wantedRow == 6){
+			row[0] = Boxes[6].getCellValue(0);
+			row[1] = Boxes[6].getCellValue(1);
+			row[2] = Boxes[6].getCellValue(2);
+			row[3] = Boxes[7].getCellValue(0);
+			row[4] = Boxes[7].getCellValue(1);
+			row[5] = Boxes[7].getCellValue(2);
+			row[6] = Boxes[8].getCellValue(0);
+			row[7] = Boxes[8].getCellValue(1);
+			row[8] = Boxes[8].getCellValue(2);
+		}else if(wantedRow == 7){
+			row[0] = Boxes[6].getCellValue(3);
+			row[1] = Boxes[6].getCellValue(4);
+			row[2] = Boxes[6].getCellValue(5);
+			row[3] = Boxes[7].getCellValue(3);
+			row[4] = Boxes[7].getCellValue(4);
+			row[5] = Boxes[7].getCellValue(5);
+			row[6] = Boxes[8].getCellValue(3);
+			row[7] = Boxes[8].getCellValue(4);
+			row[8] = Boxes[8].getCellValue(5);
 		}else{
-			currentRow[0] = Boxes[6].getCellValue(6);
-			currentRow[1] = Boxes[6].getCellValue(7);
-			currentRow[2] = Boxes[6].getCellValue(8);
-			currentRow[3] = Boxes[7].getCellValue(6);
-			currentRow[4] = Boxes[7].getCellValue(7);
-			currentRow[5] = Boxes[7].getCellValue(8);
-			currentRow[6] = Boxes[8].getCellValue(6);
-			currentRow[7] = Boxes[8].getCellValue(7);
-			currentRow[8] = Boxes[8].getCellValue(8);
+			row[0] = Boxes[6].getCellValue(6);
+			row[1] = Boxes[6].getCellValue(7);
+			row[2] = Boxes[6].getCellValue(8);
+			row[3] = Boxes[7].getCellValue(6);
+			row[4] = Boxes[7].getCellValue(7);
+			row[5] = Boxes[7].getCellValue(8);
+			row[6] = Boxes[8].getCellValue(6);
+			row[7] = Boxes[8].getCellValue(7);
+			row[8] = Boxes[8].getCellValue(8);
 		}
 
-		return allTheSame(currentRow);
+		return allTheSame(row);
 	}
 
 	public void printArray(int[] inputArray){
@@ -318,17 +324,11 @@ class Grid{
 
 		while(true){
 			if((boxIndex == 8) && (cellIndex == 8)){
-				// System.out.println("Goal: end the function");
-				// displayInfo(boxIndex, cellIndex);
-
 				printCell(boxIndex, cellIndex);
 
 				System.out.println();
 				break;
 			}else if(((boxIndex == 2) || (boxIndex == 5) || (boxIndex == 8)) && (cellIndex == 8)){
-				// System.out.println("Goal: print the BAR");
-				// displayInfo(boxIndex, cellIndex);
-
 				printCell(boxIndex, cellIndex);
 
 				System.out.println();
@@ -337,9 +337,6 @@ class Grid{
 				boxIndex++;
 				cellIndex = 0;
 			}else if(((boxIndex == 2) || (boxIndex == 5) || (boxIndex == 8)) && ((cellIndex == 2) || (cellIndex == 5) || (cellIndex == 8))){
-				// System.out.println("Goal: go to a new line");
-				// displayInfo(boxIndex, cellIndex);
-
 				printCell(boxIndex, cellIndex);
 
 				System.out.println();
@@ -347,9 +344,6 @@ class Grid{
 				boxIndex -= 2;
 				cellIndex++;
 			}else if((cellIndex == 2) || (cellIndex == 5) || (cellIndex == 8)){
-				// System.out.println("Goal: print double pipes");
-				// displayInfo(boxIndex, cellIndex);
-
 				printCell(boxIndex, cellIndex);
 
 				System.out.print("|| ");
@@ -357,9 +351,6 @@ class Grid{
 				boxIndex++;
 				cellIndex -= 2;
 			}else{
-				// System.out.println("Goal: print out a normal cell");
-				// displayInfo(boxIndex, cellIndex);
-
 				printCell(boxIndex, cellIndex);
 
 				cellIndex++;
--- README.md
+++ README.md
@@ -6,6 +6,9 @@ This is a simple sudoku game written in java.
 Need to work on:
 - A way for the user to select a certain cell and change it
 
+Issues:
+- I feel there is a better way to write the checkRow and checkColumn method in the Grid class. I cannot find a better way, help would be greatly appreciated.
+
 Please feel free to reach out to me if you have any suggestions.
 
 Email: brandon.r.weathers@gmail.com
